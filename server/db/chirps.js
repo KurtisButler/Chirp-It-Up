f3b3b3014a70e17a46fa02e38431f10211af836c
KurtisButler <burtiskitler@gmail.com>
2022-03-31 12:03:31 -0500

    syntax fix

--- server/db/chirps.js
+++ server/db/chirps.js
@@ -1,18 +1,18 @@
 import { Query } from "./index";
 
-const one = (id) => Query("Select * from chirps where chirps.id = ?", [id]);
+const one = async (id) => Query("Select * from chirps where chirps.id = ?", [id]);
 
 const all = async () => Query("Select * FROM chirps");
 
-const insert = (userid, content, location) =>
+const insert = async (userid, content, location) =>
   Query("Insert into chirps ( userid, content, location) VALUES (?, ?, ?)", 
    [userid,
     content,
     location,]);
    
-const del = (id) => Query("Delete from chirps where id = ?", [id]);
+const del = async (id) => Query("Delete from chirps where id = ?", [id]);
 
-const update = (id, content) =>
+const update = async (id, content) =>
   Query("Update chirps set content = ? where id = ?", [content, id]);
 
 export default {
